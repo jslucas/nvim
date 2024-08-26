0e8d5d4e49fcdd21da84d5baaea581ccb91c042d
Spencer <jspencerlucas@gmail.com>
2024-08-25 22:35:02 -0400

    Set title to cwd

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ require 'settings'
 require 'mappings'
 require 'autocommands'
 
+vim.o.title = true
+vim.o.titlestring = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
