83dd1337bfe15f99e9d7aa8748e536264b0b1b82
Spencer <jspencerlucas@gmail.com>
2024-08-31 16:04:26 -0400

    Configure Oil and get rid of Mini.files

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/oil.nvim',
+  keys = {
+    -- { '<leader>e', '<cmd>Oil --float<CR>', desc = 'Explorer' },
+    { '<leader>e', '<cmd>Oil<CR>', desc = 'Explorer' },
+  },
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 5,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
