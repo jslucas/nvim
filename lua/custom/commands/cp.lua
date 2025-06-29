7032c7b1acae9cb090d079d3ccb213186dd16e7c
jslucas <jspencerlucas@gmail.com>
2025-06-29 13:44:02 -0400

    refactor Cp command

--- lua/custom/commands/cp.lua
+++ lua/custom/commands/cp.lua
@@ -1,26 +1,21 @@
 -- Function to copy the file path, relative path, or filename to the + register
 local function copy_file_path(args)
+  local arg = args.arg or 'rel'
   local file_path
 
-  if args.args == 'rel' then
-    -- Get the relative path from the current working directory
-    file_path = vim.fn.expand '%:.'
-  elseif args.args == 'name' then
-    -- Get just the filename
-    file_path = vim.fn.expand '%:t'
-  else
-    -- Get the absolute path of the current file
-    file_path = vim.fn.expand '%:p'
-  end
+  local args_to_fn_name = {
+    rel = '%:.', -- relative path
+    name = '%:t', -- filename
+    abs = '%:p', -- absolute path
+  }
 
-  -- Copy the path to the + register (system clipboard)
+  file_path = vim.fn.expand(args_to_fn_name[arg])
+
   vim.fn.setreg('+', file_path)
 
-  -- Optionally, print a message to confirm
   print('Copied path: ' .. file_path)
 end
 
--- Create the custom command :Cp with an optional argument
 vim.api.nvim_create_user_command('Cp', function(args)
   copy_file_path(args)
 end, { nargs = '?' })
