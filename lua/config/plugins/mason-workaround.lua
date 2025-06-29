c2f6d63fc2698ead093b55844df4c5c1c2f7ec1d
jslucas <jspencerlucas@gmail.com>
2025-06-29 19:42:19 -0400

    Pin mason and mason-lspconfig because of 2.0 breaking changes

---
+++ lua/config/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+-- Pinned because mason-lspconfig 2.0 has breaking changes
+return {
+    { "mason-org/mason.nvim", version = "1.11.0" },
+    { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+ }
