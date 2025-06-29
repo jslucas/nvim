32fa9c0a95460b6c6458786e124e52f01dcd26f8
jslucas <jspencerlucas@gmail.com>
2025-06-29 19:42:39 -0400

    remove unused language servers

--- lua/config/plugins/nvim-lspconfig.lua
+++ lua/config/plugins/nvim-lspconfig.lua
@@ -131,23 +131,13 @@ return { -- LSP Configuration & Plugins
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       ansiblels = {},
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
       elixirls = {
         cmd = { 'elixir-ls' },
         capabilities = capabilities,
       },
-
       eslint = {
         capabilities = capabilities,
       },
-
       solargraph = {
         capabilities = capabilities,
       },
