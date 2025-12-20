5bc47eb235ab5958f095c8bcccf469a56e06d30d
jslucas <jspencerlucas@gmail.com>
2025-12-20 18:56:32 -0500

    remove mason hack

--- lua/config/plugins/mason-workaround.lua
+++
@@ -1,5 +0,0 @@
--- Pinned because mason-lspconfig 2.0 has breaking changes
-return {
-    { "mason-org/mason.nvim", version = "1.11.0" },
-    { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
- }
