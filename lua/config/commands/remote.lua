542b108688f909e04c09dea5e93b8946478a0324
jslucas <jspencerlucas@gmail.com>
2026-03-26 18:54:31 -0400

    Open GH PR for blame

--- lua/config/commands/remote.lua
+++ lua/config/commands/remote.lua
@@ -64,6 +64,53 @@ local function copy_remote_main(args)
   print('Copied main permalink: ' .. github_url)
 end
 
+-- Opens the GitHub PR associated with the commit that last touched the current line
+local function open_pr()
+  local is_git_repo = vim.fn.system('git rev-parse --is-inside-work-tree'):gsub('%s+', '') == 'true'
+  if not is_git_repo then
+    print 'Not a git repository'
+    return
+  end
+
+  local file = vim.fn.expand '%:.'
+  local line = vim.fn.line '.'
+  local blame_output = vim.fn.system(string.format('git blame -l -L %d,%d -- %s', line, line, file))
+  local commit = blame_output:match '^(%x+)'
+
+  if not commit or commit:match '^0+$' then
+    print 'No commit found (uncommitted line)'
+    return
+  end
+
+  local git_remote = vim.fn.system('git config --get remote.origin.url')
+    :gsub('%s+', '')
+    :gsub('git@github.com:', 'https://github.com/')
+    :gsub('%.git$', '')
+  local owner_repo = git_remote:match 'github.com/(.+)$'
+  if not owner_repo then
+    print 'Could not determine GitHub repo'
+    return
+  end
+
+  -- gh returns the PR URL that merged this commit (or empty if none)
+  local pr_url = vim.fn
+    .system(string.format('gh pr list --search %s --state merged --json url --jq ".[0].url" --repo %s', commit, owner_repo))
+    :gsub('%s+', '')
+
+  if pr_url == '' or pr_url == 'null' then
+    print('No merged PR found for commit ' .. commit:sub(1, 8))
+    return
+  end
+
+  local open_cmd = 'open'
+  if vim.fn.has 'unix' == 1 and vim.fn.has 'mac' == 0 then
+    open_cmd = 'xdg-open'
+  end
+  vim.fn.system(string.format("%s '%s'", open_cmd, pr_url))
+  print('Opened PR: ' .. pr_url)
+end
+
 vim.api.nvim_create_user_command('OpenRemote', open_remote, { range = true })
 vim.api.nvim_create_user_command('CopyRemote', copy_remote, { range = true })
 vim.api.nvim_create_user_command('CopyRemoteMain', copy_remote_main, { range = true })
+vim.api.nvim_create_user_command('OpenPR', open_pr, {})
